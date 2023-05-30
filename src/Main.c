35dd6fa235077814d4d3fc49aac4f82d1080e015
Palanath <pala@dusttoash.org>
2023-05-30 14:48:22 -0500

    Create empty program with SDL includes

---
+++ src/Main.c
@@ -0,0 +1,14 @@
+/*
+ * Main.c
+ *
+ *  Created on: May 30, 2023
+ *      Author: Palanath
+ */
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_video.h>
+
+int main(int argc, char *argv[]) {
+
+	return 0;
+}
