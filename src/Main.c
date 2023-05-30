cde570772e6244239a04276bf66f322da1407f72
Palanath <pala@dusttoash.org>
2023-05-30 16:38:11 -0500

    Make application show a window for 2.5 sec

--- src/Main.c
+++ src/Main.c
@@ -2,6 +2,15 @@
 #include <SDL2/SDL_video.h>
 
 int main(int argc, char *argv[]) {
+	SDL_Init(SDL_INIT_EVERYTHING);
 
+	SDL_Window *win;
+	SDL_Renderer *renderer;
+	SDL_CreateWindowAndRenderer(600, 400, 0, win, renderer);
+
+	// Delay for testing (the window should show up, but be frozen, for 2.5 seconds).
+	SDL_Delay(2500);
+
+	SDL_Quit();
 	return 0;
 }
