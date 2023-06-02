37942d18f2764aea3f7bea96624404dbe7a96605
LukeSkywalker6 <primitiveforms@gmail.com>
2023-06-02 20:39:03 +0000

    Seperate function for SDL init, removed image loading, and added skeleton for input

--- src/Main.c
+++ src/Main.c
@@ -8,56 +8,55 @@
 #define SCREEN_WIDTH 160
 #define SCREEN_HEIGHT 96
 
-void load_img(const char *image_path);
+typedef struct point {
+  int y;
+  int x;
+} point;
+
+typedef struct entity {
+  point pos;
+} entity;
+
+int init_sdl(SDL_Window *window, SDL_Renderer *renderer);
 
 int main(int argc, char *argv[]) {
+  //Placeholder variables
+  int c = 0;
 
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengl");
-	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
+  //Initializing SDL
+  SDL_Window *window;
+  SDL_Renderer *renderer;
 
-	SDL_Window *window = SDL_CreateWindow(WINDOW_TITLE,
-	SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-	WINDOW_WIDTH, WINDOW_HEIGHT,
-			WINDOW_FULLSCREEN ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0);
-
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
-			SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED
-					| SDL_RENDERER_TARGETTEXTURE);
-
-	SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-	SDL_Texture *screenTexture = SDL_CreateTexture(renderer,
-			SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH,
-			SCREEN_HEIGHT);
-
+  init_sdl(window, renderer);
 	SDL_RaiseWindow(window);
 
-	SDL_Surface *image = SDL_LoadBMP("src/test.bmp");
-
-	/* Let the user know if the file failed to load */
-	if (!image) {
-		printf("Failed to load image at %s: %s\n", "src/test.bmp",
-				SDL_GetError());
-	} else {
-		printf("Loaded image");
-	}
-	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, image);
-
+  //Main loop
 	int running = 1;
 	SDL_Event event;
 	while (running) {
 		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT) {
-				running = 0;
+      switch (event.type) {
+        case SDL_WINDOWEVENT:
+          //DRAW GAME ON ANY WINDOW EVENT
+          break;
+        case SDL_KEYDOWN:
+          switch (event.key.keysym.sym) {
+            case SDLK_w:
+              printf("%d\n", ++c);
+              break;
+            case SDLK_s:
+              printf("%d\n", --c);
+              break;
+          }
+          break;
+        case SDL_QUIT:
+          running = 0;
+          break;
 			}
-
-			SDL_RenderCopy(renderer, texture, NULL, NULL);
-			SDL_RenderPresent(renderer);
 		}
 	}
 
-	SDL_DestroyTexture(screenTexture);
+  //Cleanup
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
@@ -65,18 +64,21 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-void load_img(const char *image_path) {
-	SDL_Surface *image = SDL_LoadBMP(image_path);
+int init_sdl(SDL_Window *window, SDL_Renderer *renderer) {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengl");
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
 
-	/* Let the user know if the file failed to load */
-	if (!image) {
-		printf("Failed to load image at %s: %s\n", image_path, SDL_GetError());
-	} else {
-		printf("Loaded image");
-	}
+	window = SDL_CreateWindow(WINDOW_TITLE,
+      SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+      WINDOW_WIDTH, WINDOW_HEIGHT,
+      WINDOW_FULLSCREEN ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0);
 
-	/* Do something with image here. */
+	renderer = SDL_CreateRenderer(window, -1,
+			SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED
+					| SDL_RENDERER_TARGETTEXTURE);
 
-	/* Make sure to eventually release the surface resource */
-	SDL_FreeSurface(image);
+	SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  return 1;
 }
