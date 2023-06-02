b4243e84455f91148441551d854c5f4a5a9ccf73
Palanath <pala@dusttoash.org>
2023-06-02 16:42:21 -0500

    Update Main.c

--- src/Main.c
+++ src/Main.c
@@ -10,56 +10,65 @@
 #define SCREEN_HEIGHT 96
 
 typedef struct point {
-  int y;
-  int x;
+	int y;
+	int x;
 } point;
 
 typedef struct entity {
-  point pos;
+	point pos;
 } entity;
 
-int init_sdl(SDL_Window *window, SDL_Renderer *renderer);
+struct InitializationResults {
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+};
+
+struct InitializationResults init_sdl();
 
 int main(int argc, char *argv[]) {
-  //Placeholder variables
-  int c = 0;
+	//Placeholder variables
+	int c = 0;
 
-  //Initializing SDL
-  SDL_Window *window;
-  SDL_Renderer *renderer;
+	//Initializing SDL
+	SDL_Window *window;
+	SDL_Renderer *renderer;
 
-  if (!init_sdl(window, renderer))
-    exit(1);
+	struct InitializationResults results = init_sdl();
+
+	window = results.window;
+	renderer = results.renderer;
+
+	printf("%p", window);
 
 	SDL_RaiseWindow(window);
 
-  //Main loop
+	//Main loop
 	int running = 1;
 	SDL_Event event;
 	while (running) {
 		while (SDL_PollEvent(&event)) {
-      switch (event.type) {
-        case SDL_WINDOWEVENT:
-          //DRAW GAME ON ANY WINDOW EVENT
-          break;
-        case SDL_KEYDOWN:
-          switch (event.key.keysym.sym) {
-            case SDLK_w:
-              printf("%d\n", ++c);
-              break;
-            case SDLK_s:
-              printf("%d\n", --c);
-              break;
-          }
-          break;
-        case SDL_QUIT:
-          running = 0;
-          break;
+			switch (event.type) {
+			case SDL_WINDOWEVENT:
+				//DRAW GAME ON ANY WINDOW EVENT
+				break;
+			case SDL_KEYDOWN:
+				switch (event.key.keysym.sym) {
+				case SDLK_w:
+					printf("%d\n", ++c);
+					break;
+				case SDLK_s:
+					printf("%d\n", --c);
+					break;
+				}
+				break;
+			case SDL_QUIT:
+				running = 0;
+				break;
 			}
 		}
 	}
 
-  //Cleanup
+	//Cleanup
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
@@ -67,30 +76,30 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-int init_sdl(SDL_Window *window, SDL_Renderer *renderer) {
+struct InitializationResults init_sdl() {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengl");
 	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
+	struct InitializationResults res;
 
-	window = SDL_CreateWindow(WINDOW_TITLE,
-      SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-      WINDOW_WIDTH, WINDOW_HEIGHT,
-      WINDOW_FULLSCREEN ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0);
-  if (window == NULL) {
-    fprintf(stderr, "Window couldn't be created\n");
-    return 0;
-  }
+	res.window = SDL_CreateWindow(WINDOW_TITLE,
+	SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+	WINDOW_WIDTH, WINDOW_HEIGHT,
+	WINDOW_FULLSCREEN ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0);
+	if (res.window == NULL) {
+		fprintf(stderr, "Window couldn't be created\n");
+		exit(1);
+	}
 
-	renderer = SDL_CreateRenderer(window, -1,
+	res.renderer = SDL_CreateRenderer(res.window, -1,
 			SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED
 					| SDL_RENDERER_TARGETTEXTURE);
-  if (renderer == NULL) {
-    fprintf(stderr, "Renderer couldn't be created\n");
-    return 0;
-  }
+	if (res.renderer == NULL) {
+		fprintf(stderr, "Renderer couldn't be created\n");
+		exit(1);
+	}
 
+	SDL_RenderSetLogicalSize(res.renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-	SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-  return 1;
+	return res;
 }
